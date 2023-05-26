02c5725686909c821660502492d2218adfa6552d
Tamyris968 <tamyrisbenini@gmail.com>
2023-05-26 21:25:53 +0900

    AtualizaçãoDia26/05

--- www/js/motor.js
+++ www/js/motor.js
@@ -3,11 +3,33 @@ alert("Funcionou!")
 var nome
 var selecao
 var selecaodisciplina
+var selecaomencao
 
 function cadastrar() {
-    nome = document.getElementeById('inputnome').value
-    document.getElementeById('nomealuno').textContent = nome
+    nome = document.getElementById('inputnome').value
+    document.getElementById('nomealuno').textContent = nome
 
-    selecao = document.getElementeById('disciplina')
-    selecaodisciplina = selecao.option[selecao.selecteIndex].value
+    selecao = document.getElementById('disciplina')
+    selecaodisciplina = selecao.options[selecao.selectedIndex].value
+
+    selecao = document.getElementById('mencao')
+    selecaomencao = selecao.options[selecao.selectedIndex].value
+
+    alert(selecaodisciplina)
+    alert(selecaomencao)
+    alert(nome)
+
+    localStroge.setItem(selecaodisciplina, selecaomencao)
+}
+
+function listar() {
+    var exibirmencao
+
+    selecao = document.getElementById('disciplina')
+    selecaodisciplina = selecao.options[selecao.selectedIndex].value
+
+    exibirmencao = localStroge.getItem(selecaodisciplina)
+
+    document.getElementById('exibirdisciplina').textContent = selecaodisciplina
+    document.getElementById('exibirmencao').textContent = exibirmencao
 }
